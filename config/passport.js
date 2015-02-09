20bede61a0ac8f41fb7aa38d05a362786de3aa0b
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-09 18:36:57 +0100

    Passport locañ 1 Incluir dependencias con npm install passport-local --save 2 nuevo directorio en config llamado estrategies 3Nuevo archivo local.js en estrategies donde implentamos la estrategia local 3 configuracion de passport.js en config 4

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,20 @@
 /**
  * Created by Carlos on 09/02/2015.
  */
+var passport = require('passport'),
+    mongoose = require('mongoose');
+
+module.exports = function(){
+    var User = mongoose.model('User');
+    
+    passport.serializeUser(function(user,done){
+        done(null, user.id);        
+    });
+    
+    passport.deserializeUser(function(id,done){
+        User.findOne({_id:id},'-password -salt',function(err, user){
+            done(err, user);            
+        });     
+    });
+    require('./strategies/local.js')();
+};
--- config/strategies/local.js
+++ config/strategies/local.js
@@ -1,3 +1,29 @@
 /**
  * Created by Carlos on 09/02/2015.
  */
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    User = require('mongoose').model('User');
+
+module.exports = function(){
+    passport.use(new LocalStrategy(function(username, password, done){
+        User.findOne({
+            username: username
+        },function(err,user){
+            if(err){
+                return done(err);                
+            }
+            if(!user){
+                return done(null,false,{
+                    message: 'Usuario desconocido'                    
+                });                
+            }
+            if(!user.authenticate(password)){
+                return done(null,false,{
+                    message: 'Password no válido'                    
+                })                
+            }
+            return done(null,user);            
+        });      
+    }));    
+};
