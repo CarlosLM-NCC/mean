1dcc78694f208bc4e6f9dbdd57cc8c64876b0357
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-09 18:47:39 +0100

    Passport local

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var mongoose = require('./config/mongoose'),
     express = require('./config/express'),
-    Passport = require('passport');
+    passport = require('./config/passport');
 
 var db = mongoose();
 var app = express();
-var passport = Passport();
+var passport = passport();
 app.listen(3000);
 module.exports = app;
 
