1b52ec7b0ea42d2bf27e31bb8a6664b87785bb8d
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-11 13:22:18 +0100

    configurar angular-roues bowel install angular-roues --save

--- public/application.js
+++ public/application.js
@@ -3,7 +3,11 @@
  */
 var mainApplicationModuleName= 'mean';
 var mainApplicationModule= angular.module(mainApplicationModuleName,['ngRoute','example']);
-
+//Configura el hashbang
+mainApplicationModule.config(['$locationProvider',
+function($locationProvider){
+    $locationProvider.hashPrefix('!');    
+}]);
 angular.element(document).ready(function(){
     angular.bootstrap(document, [mainApplicationModuleName])
 });
