33fbc795f623dc59f0100060f8724c8e93f962de
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-11 13:05:08 +0100

    module example en angular

---
+++ public/example/controllers/example.client.controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Carlos on 11/02/2015.
+ */
+angular.module('example').controller('ExampleController',['$scope',
+  function($scope){
+      $scope.name = 'Aplicación MEAN '
+  }
+]);
