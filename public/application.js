68ff0864fdd9db2fe17d8f9aadd8eb35dc2e1b4f
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-11 01:00:32 +0100

    module example en angular

--- public/application.js
+++ public/application.js
@@ -2,7 +2,7 @@
  * Created by Carlos on 11/02/2015.
  */
 var mainApplicationModuleName= 'mean';
-var mainApplicationModule= angular.module(mainApplicationModuleName,[]);
+var mainApplicationModule= angular.module(mainApplicationModuleName,['example']);
 
 angular.element(document).ready(function(){
     angular.bootstrap(document, [mainApplicationModuleName])
