65d72bb81295541360e7b23cfde29a61b32ac3e5
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-11 23:18:13 +0100

    configurar angular-services autentificacion usando services

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -3,6 +3,7 @@
  */
 angular.module('example').controller('ExampleController',['$scope', 'Authentication',
   function($scope, Authentication){
-      $scope.name = Authentication.user ? Authentication.user.lastName : 'Aplicación Mean';
+      $scope.name = Authentication.user ? Authentication.user.firstName : 'Aplicación Mean';
+      $scope.fullName = Authentication.user ? Authentication.user.firstName +" "+ Authentication.user.lastName : 'Aplicación Mean';
   }
 ]);
