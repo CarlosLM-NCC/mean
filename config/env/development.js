0d88260bdf04a44c35a80df7041af25dfb6c0205
NCC-CarlosLM <c.lopezmotos@gmail.com>
2015-02-12 11:12:25 +0100

    Crear Módulo Mean CRUD       Crear Modelo mongoose

--- config/env/development.js
+++ config/env/development.js
@@ -5,8 +5,8 @@ module.exports={
     db: 'mongodb://localhost/mean',
     sessionSecret : "developmentSessionSecret",
     google: {
-        clientID: 'el clientid de la aplicacion google',
-        clientSecret: 'el clientSecret de la aplicacion goolgle',
+        clientID: '522359631710-2o1pnj2f9b9jn0ha7l6r0jmbdulvsbtf.apps.googleusercontent.com',
+        clientSecret: '6DiDn4LwJxQzoaA8SI-47yTq',
         callbackURL: 'http://localhost:3000/oauth/google/callback'
     }
 };
